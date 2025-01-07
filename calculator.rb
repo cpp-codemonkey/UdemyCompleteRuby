78bd18ce43029550a09447be6019dc6c9cf708d2
cpp-codemonkey <32357883+cpp-codemonkey@users.noreply.github.com>
2025-01-07 16:22:25 +0000

    modded calculator.rb to use functions instead of embedded calculations

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,23 @@
+def mult(x, y)
+	return x.to_f * y.to_f
+end
+
+def divide(x, y)
+	return x.to_f / y.to_f
+end
+
+def add(x, y)
+	return x.to_f + y.to_f
+end
+
+def subtract(x, y)
+	return x.to_f - y.to_f
+end
+
+def modulus(x, y)
+	return x.to_f % y.to_f
+end
+
 puts("Simple calculator")
 25.times{print("-")}
 puts()
@@ -12,20 +32,14 @@ op = gets.chomp()
 
 case op
 	when "+"
-		puts("The first number added to the second is #{num_1.to_f() + num_2.to_f()}")
+		puts("The first number added to the second is #{add(num_1, num_2)}")
 	when "-"
-		puts("The first number minus the second is #{num_1.to_f() - num_2.to_f()}")
+		puts("The first number minus the second is #{minus(num_1, num_2)}")
 	when "*"
-		puts("The first number multiplied by the second is #{num_1.to_f() * num_2.to_f()}")
+		puts("The first number multiplied by the second is #{mult(num_1, num_2)}")
 	when "/"
-		puts("The first number divided by the second is #{num_1.to_f() / num_2.to_f()}")
-		puts("The first number modulus the second is #{num_1.to_f() % num_2.to_f()}")
+		puts("The first number divided by the second is #{divide(num_1, num_2)}")
+		puts("The first number modulus the second is #{modulus(num_1, num_2)}")
 else
 	puts("#{op} is not a valid operator")
 end
-
-# puts("The first number added to the second is #{num_1.to_f() + num_2.to_f()}")
-# puts("The first number minus the second is #{num_1.to_f() - num_2.to_f()}")
-# puts("The first number multiplied by the second is #{num_1.to_f() * num_2.to_f()}")
-# puts("The first number divided by the second is #{num_1.to_f() / num_2.to_f()}")
-# puts("The first number modulus the second is #{num_1.to_f() % num_2.to_f()}")
